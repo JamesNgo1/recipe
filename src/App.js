b9272c03a7123436ea5f365516867e956b839a35
James Ngo <Jamestngo02@gmail.com>
2023-07-19 04:15:08 -0400

    fix initial

--- src/App.js
+++ src/App.js
@@ -2,71 +2,48 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Recipe from "./Recipe";
 
-// remind to keep react hooks not a top level but within a function component
-
-function App() {
-  /* My custom keys so remidner to use environment tools */
+const App = () => {
   const APP_ID = "7e85eb37";
-  const APP_KEY = "2d0f4b31031aa9cd87c8a0eaa8fcff16";
+  const APP_KEYS = "2d0f4b31031aa9cd87c8a0eaa8fcff16";
 
-  const Request = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipe] = useState([]); //holds the array of recipe of search item
+  const [search, setSearch] = useState(""); //state for the search
 
-  //good practice to use className for good practice
-
-  //use effect function that runs
-  //takes arrow functio nas paramter
-
-  //use effect
-
-  //we do not know how long the ifnormation comes back so use await everytime you have a promise . external
-
-  //promise : is .then
-
-  //exampel shown in the video is nicer and simple than the promose did in weather app
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
+  //interpolate the variable id and keys within the request
+  const REQUEST = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEYS}`;
 
   useEffect(() => {
     getRecipes();
   }, []);
 
   const getRecipes = async () => {
-    const response = await fetch(Request);
-
+    const response = await fetch(REQUEST);
     const data = await response.json();
-    setRecipes(data.hits);
+    setRecipe(data.hits);
     console.log(data.hits);
   };
-
-  function updateSearch(e) {
-    setSearch(e.target.value);
-  }
 
   return (
     <div className="App">
       <form className="search-form">
-        <input
-          className="search-bar"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        ></input>
+        <input className="search-bar" type="text"></input>
         <button className="search-button" type="submit">
           search
         </button>
       </form>
-      {recipes.map((recipe) => (
-        //now we need to add props
-        <Recipe
-          key={recipe.recipe.label}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
+
+      {recipes.map((recipe) => {
+        return (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+          />
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
